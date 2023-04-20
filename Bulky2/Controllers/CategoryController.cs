5b49830961d4154e9ddafbea56e1b1991cb60e54
Tavo Soto <gsoto@itx.com>
2023-04-20 10:03:44 -0500

    Add Category table via migrations with controller and view

---
+++ Bulky2/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky2.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Bulky2/Models/Category.cs
+++ Bulky2/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace Bulky2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bulky2.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
