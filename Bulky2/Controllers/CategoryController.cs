5e24d8dfac73b250180be672e297f85563330f4d
Tavo Soto <gsoto@itx.com>
2023-04-20 11:37:02 -0500

    Adding Category data to view

--- Bulky2/Controllers/CategoryController.cs
+++ Bulky2/Controllers/CategoryController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky2.Data;
+using Bulky2.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bulky2.Controllers
 {
     public class CategoryController : Controller
     {
+        // NOTE: Class param for DB context to share in between different
+        //       controller methods.
+        private readonly ApplicationDbContext _db;
+
+        // Class's Constructor
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // Index Action
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
