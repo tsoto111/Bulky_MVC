9ae07b872eb7411ba5e13934ed5b42cf5e54b82a
Tavo Soto <gsoto@itx.com>
2023-04-20 11:15:27 -0500

    Add Category db seeded data

--- Bulky2/Data/ApplicationDbContext.cs
+++ Bulky2/Data/ApplicationDbContext.cs
@@ -11,5 +11,18 @@ namespace Bulky2.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        // NOTE: Entity Framework's OnModelCreating logic which we will overwrite to 
+        //       seed our own data.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+            );
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
