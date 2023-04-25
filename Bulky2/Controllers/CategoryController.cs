7488e86dd844b483edb5cd7a26c068b01383488e
Tavo Soto <gsoto@itx.com>
2023-04-25 11:11:29 -0500

    Add the New action handler for Create action payload

--- Bulky2/Controllers/CategoryController.cs
+++ Bulky2/Controllers/CategoryController.cs
@@ -23,9 +23,18 @@ namespace Bulky2.Controllers
             return View(objCategoryList);
         }
 
+        // Create Action
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
