dc14e4d11e197dbd8b35a5849974f958b0bdf580
Tavo Soto <gsoto@itx.com>
2023-05-10 21:06:59 -0500

    Add new model Interface Repository logic to DatAccess

---
+++ DataAccess/Repository/InterfaceRepository/InterfaceRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.InterfaceRepository
+{
+    internal interface InterfaceRepository<T> where T : class
+    {
+        // T - Represents any generic Model we will operate on, for example Category.
+        IEnumerable<T> GetAll();
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
