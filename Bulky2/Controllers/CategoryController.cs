ee49f19b852f943cb6a3dafee04108ef420a1152
Tavo Soto <gsoto@itx.com>
2023-05-16 12:31:30 -0500

    Using new Category repository via Category controller instead of DB context

--- Bulky2/Controllers/CategoryController.cs
+++ Bulky2/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.InterfaceRepository;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,14 +9,14 @@ namespace Bulky2.Controllers
     {
         // NOTE: Class param for DB context to share in between different
         //       controller methods.
-        private readonly ApplicationDbContext _db;
+        private readonly ICategoryRepository _categoryRepo;
 
         // ============================================
         // Class's Constructor
         // ============================================
-        public CategoryController(ApplicationDbContext db)
+        public CategoryController(ICategoryRepository db)
         {
-            _db = db;
+            _categoryRepo = db;
         }
 
         // ============================================
@@ -23,7 +24,7 @@ namespace Bulky2.Controllers
         // ============================================
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _db.Categories.ToList();
+            List<Category> objCategoryList = _categoryRepo.GetAll().ToList();
             return View(objCategoryList);
         }
 
@@ -48,8 +49,8 @@ namespace Bulky2.Controllers
 
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
-                _db.SaveChanges();
+                _categoryRepo.Add(obj);
+                _categoryRepo.Save();
                 TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
@@ -67,7 +68,7 @@ namespace Bulky2.Controllers
                 return NotFound();
             }
 
-            Category? categoryFromDb = _db.Categories.Find(ID);
+            Category? categoryFromDb = _categoryRepo.GetFirstOrDefault(u => u.Id == ID);
             // Category? categoryFromDb1 = _db.Categories.FirstOrDefault(c => c.Id == ID); // Another way to query a single record by ID.
             // Category? categoryFromDb2 = _db.Categories.Where(u => u.Id === ID).FirstOrDefault(); // Another way to query a single record by ID.
             if (categoryFromDb == null)
@@ -83,8 +84,8 @@ namespace Bulky2.Controllers
         {   
             if (ModelState.IsValid)
             {
-                _db.Categories.Update(obj);
-                _db.SaveChanges();
+                _categoryRepo.Update(obj);
+                _categoryRepo.Save();
                 TempData["success"] = "Category edited successfully";
                 return RedirectToAction("Index");
             }
@@ -102,7 +103,7 @@ namespace Bulky2.Controllers
                 return NotFound();
             }
 
-            Category? categoryFromDb = _db.Categories.Find(ID);
+            Category? categoryFromDb = _categoryRepo.GetFirstOrDefault(u => u.Id == ID);
             if (categoryFromDb == null)
             {
                 return NotFound();
@@ -119,14 +120,14 @@ namespace Bulky2.Controllers
                 return NotFound();
             }
 
-            Category? categoryFromDb = _db.Categories.Find(ID);
+            Category? categoryFromDb = _categoryRepo.GetFirstOrDefault(u => u.Id == ID);
             if (categoryFromDb == null)
             {
                 return NotFound();
             }
 
-            _db.Categories.Remove(categoryFromDb);
-            _db.SaveChanges(true);
+            _categoryRepo.Remove(categoryFromDb);
+            _categoryRepo.Save();
             TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
