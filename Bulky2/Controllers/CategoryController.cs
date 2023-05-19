9cfeb311252e2a17358d8c9f01ce9bf628420b1b
Tavo Soto <gsoto@itx.com>
2023-05-19 13:08:00 -0500

    Add UnitOfWork repo to handle database CRUD logic for different models

--- Bulky2/Controllers/CategoryController.cs
+++ Bulky2/Controllers/CategoryController.cs
@@ -1,22 +1,22 @@
-﻿using Bulky.DataAccess.Data;
+﻿using Bulky.DataAccess.Repository;
 using Bulky.DataAccess.Repository.InterfaceRepository;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Bulky2.Controllers
+namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
         // NOTE: Class param for DB context to share in between different
         //       controller methods.
-        private readonly ICategoryRepository _categoryRepo;
+        private readonly IUnitOfWork _unitOfWork;
 
         // ============================================
         // Class's Constructor
         // ============================================
-        public CategoryController(ICategoryRepository db)
+        public CategoryController(IUnitOfWork unitOfWork)
         {
-            _categoryRepo = db;
+            _unitOfWork = unitOfWork;
         }
 
         // ============================================
@@ -24,7 +24,7 @@ namespace Bulky2.Controllers
         // ============================================
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _categoryRepo.GetAll().ToList();
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
             return View(objCategoryList);
         }
 
@@ -49,8 +49,8 @@ namespace Bulky2.Controllers
 
             if (ModelState.IsValid)
             {
-                _categoryRepo.Add(obj);
-                _categoryRepo.Save();
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
                 TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
@@ -68,7 +68,7 @@ namespace Bulky2.Controllers
                 return NotFound();
             }
 
-            Category? categoryFromDb = _categoryRepo.GetFirstOrDefault(u => u.Id == ID);
+            Category? categoryFromDb = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == ID);
             // Category? categoryFromDb1 = _db.Categories.FirstOrDefault(c => c.Id == ID); // Another way to query a single record by ID.
             // Category? categoryFromDb2 = _db.Categories.Where(u => u.Id === ID).FirstOrDefault(); // Another way to query a single record by ID.
             if (categoryFromDb == null)
@@ -84,8 +84,8 @@ namespace Bulky2.Controllers
         {   
             if (ModelState.IsValid)
             {
-                _categoryRepo.Update(obj);
-                _categoryRepo.Save();
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
                 TempData["success"] = "Category edited successfully";
                 return RedirectToAction("Index");
             }
@@ -103,7 +103,7 @@ namespace Bulky2.Controllers
                 return NotFound();
             }
 
-            Category? categoryFromDb = _categoryRepo.GetFirstOrDefault(u => u.Id == ID);
+            Category? categoryFromDb = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == ID);
             if (categoryFromDb == null)
             {
                 return NotFound();
@@ -120,14 +120,14 @@ namespace Bulky2.Controllers
                 return NotFound();
             }
 
-            Category? categoryFromDb = _categoryRepo.GetFirstOrDefault(u => u.Id == ID);
+            Category? categoryFromDb = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == ID);
             if (categoryFromDb == null)
             {
                 return NotFound();
             }
 
-            _categoryRepo.Remove(categoryFromDb);
-            _categoryRepo.Save();
+            _unitOfWork.Category.Remove(categoryFromDb);
+            _unitOfWork.Save();
             TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
--- DataAccess/Repository/InterfaceRepository/InterfaceCategoryRepository.cs
+++ DataAccess/Repository/InterfaceRepository/InterfaceCategoryRepository.cs
@@ -5,6 +5,5 @@ namespace Bulky.DataAccess.Repository.InterfaceRepository
     public interface ICategoryRepository : IRepository<Category>
     {
         void Update(Category obj);
-        void Save();
     }
 }
