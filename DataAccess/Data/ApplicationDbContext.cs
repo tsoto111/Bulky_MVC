e17f5063565f30cee79446f797f5695f29a9c23c
Tavo Soto <gsoto@itx.com>
2023-05-24 12:20:24 -0500

    Add foreign key for Category Product relationship

--- DataAccess/Data/ApplicationDbContext.cs
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -37,6 +37,7 @@ namespace Bulky.DataAccess.Data
                     Price = 90,
                     Price50 = 85,
                     Price100 = 80,
+                    CategoryId = 2,
                 },
                 new Product
                 {
@@ -49,6 +50,7 @@ namespace Bulky.DataAccess.Data
                     Price = 30,
                     Price50 = 25,
                     Price100 = 20,
+                    CategoryId = 1,
                 },
                 new Product
                 {
@@ -61,6 +63,7 @@ namespace Bulky.DataAccess.Data
                     Price = 50,
                     Price50 = 40,
                     Price100 = 35,
+                    CategoryId = 1,
 
                 },
                 new Product
@@ -74,6 +77,7 @@ namespace Bulky.DataAccess.Data
                     Price = 65,
                     Price50 = 60,
                     Price100 = 55,
+                    CategoryId = 3,
 
                 },
                 new Product
@@ -87,6 +91,7 @@ namespace Bulky.DataAccess.Data
                     Price = 27,
                     Price50 = 25,
                     Price100 = 20,
+                    CategoryId = 3,
 
                 },
                 new Product
@@ -100,6 +105,7 @@ namespace Bulky.DataAccess.Data
                     Price = 23,
                     Price50 = 22,
                     Price100 = 20,
+                    CategoryId = 3,
 
                 }
             );
